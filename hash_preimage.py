f6c6055f21f3087983b914a383505570574c8f5b
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-05-24 04:02:05 +0100

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -1,1 +1,32 @@
+import hashlib
+import os
 
+def hash_preimage(target_string):
+    if not all( [x in '01' for x in target_string ] ):
+        print( "Input should be a string of bits" )
+        return
+    nonce = b'\x00'
+
+    while True:
+        x = os.urandom(len(target_string))
+        x_hash = hashlib.sha256(x).hexdigest()
+        x_hash_binary = hex2binary(x_hash)
+
+        for i in range(len(target_string)):
+            if x_hash_binary[len(x_hash_binary) - i - 1] == target_string[len(target_string) - i - 1] and i == len(target_string)-1:
+                print(x_hash_binary)
+                return (x)
+            if x_hash_binary[len(x_hash_binary) - i - 1] == target_string[len(target_string) - i - 1]:
+                continue
+            else:
+                break
+
+def hex2binary(hex):
+    end_length = len(hex) * 4
+
+    hex_as_int = int(hex, 16)
+    hex_as_binary = bin(hex_as_int)
+    padded_binary = hex_as_binary[2:].zfill(end_length)
+    return padded_binary
+
+hash_preimage('00101011101001001010')
