ba220853d49560a79d76d2e182912895f85b8dec
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-05-24 01:54:07 +0100

    Update hash_collision.py

--- hash_collision.py
+++ hash_collision.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import random
 import secrets
 
 
@@ -7,7 +8,7 @@ def hash_collision(k):
     if not isinstance(k, int):
         print("hash_collision expects an integer")
         return (b'\x00', b'\x00')
-    if k < 0:
+    if k <= 0:
         print("Specify a positive number of bits")
         return (b'\x00', b'\x00')
 
@@ -16,13 +17,17 @@ def hash_collision(k):
 
 
     while True:
-        x = b"\x00" + secrets.token_bytes(4) + b"\x00"
-        y = b"\x00" + secrets.token_bytes(4) + b"\x00"
+        x = random.randbytes(4)
+        y = random.randbytes(4)
         x_hash = hashlib.sha256(x).hexdigest()
         y_hash = hashlib.sha256(y).hexdigest()
 
         for i in range(k):
             if x_hash[63-i] == y_hash[63-i] and i==k-1:
+                print(x)
+                print(y)
+                print(x_hash)
+                print(y_hash)
                 return (x, y)
             if x_hash[63-i] == y_hash[63-i]:
                 continue
