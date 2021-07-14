83d860e4c9cefa12cd9c1f1a7744f2c03fde83bf
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-07-14 04:44:42 +0100

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -3,47 +3,32 @@ from flask_restful import Api
 import json
 import eth_account
 import algosdk
-
-
 app = Flask(__name__)
 api = Api(app)
 app.url_map.strict_slashes = False
-
-def verifyAlgorand(request):
-    valid_alg = False
-    payload = request['payload']
-    algo_sig_str = request['sig']
-    algo_pk = payload['pk']
-    payload = json.dumps(payload)
-
-    if algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk):
-        valid_alg = True
-    return valid_alg
-
-
-def verifyEtherium(request):
-    valid_eth = False
-
-    eth_pk = request['payload']['pk']
-    payload = request['payload']
-
-    payload = json.dumps(payload)
-    eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-    if eth_account.Account.recover_message(eth_encoded_msg, signature=request['sig']) == eth_pk:
-        valid_eth = True
-
-    return valid_eth
-
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     result = False #Should only be true if signature validates
     request = request.get_json(silent=True)
 
-    if request['payload']['platform'] == 'Ethereum' and verifyEtherium(request):
-        result = True
-    else:
-        if request['payload']['platform'] == 'Algorand' and verifyAlgorand(request):
+    if request['payload']['platform'] == 'Ethereum':
+        eth_pk = request['payload']['pk']
+        payload = request['payload']
+
+        payload = json.dumps(payload)
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        if eth_account.Account.recover_message(eth_encoded_msg, signature=request['sig']) == eth_pk:
+            result = True
+
+    if request['payload']['platform'] == 'Algorand':
+
+        payload = request['payload']
+        algo_sig_str = request['sig']
+        algo_pk = payload['pk']
+        payload = json.dumps(payload)
+
+        if algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk):
             result = True
 
     # Check if signature is valid
