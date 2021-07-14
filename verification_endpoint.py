dce4a7edfc7eb5c2074a27c7b4a55a4e36282b7d
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-07-14 04:52:53 +0100

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -4,51 +4,17 @@ import json
 import eth_account
 import algosdk
 
-
 app = Flask(__name__)
 api = Api(app)
 app.url_map.strict_slashes = False
 
-def verifyEth(request):
-    valid_eth = False
-
-    eth_pk = request['payload']['pk']
-    payload = request['payload']
-
-    payload = json.dumps(payload)
-    eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-    if eth_account.Account.recover_message(eth_encoded_msg, signature=request['sig']) == eth_pk:
-        print("Eth sig verifies!")
-        valid_eth = True
-
-    return valid_eth
-
-def verifyAlg(request):
-    valid_alg = False
-    payload = request['payload']
-    algo_sig_str = request['sig']
-    algo_pk = payload['pk']
-    payload = json.dumps(payload)
-
-    if algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk):
-        print("Algo sig verifies!")
-        valid_alg = True
-    return valid_alg
-
-
 @app.route('/verify', methods=['GET','POST'])
 def verify():
-    result = False #Should only be true if signature validates
-    request = request.get_json(silent=True)
+    content = request.get_json(silent=True)
 
-    if request['payload']['platform'] == 'Ethereum' and verifyEth(request):
-        result = True
-    else:
-        if request['payload']['platform'] == 'Algorand' and verifyAlg(request):
-            result = True
-
+    #Check if signature is valid
+    result = True #Should only be true if signature validates
     return jsonify(result)
 
-
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(port='5002')
