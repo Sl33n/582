3f10a771aadfb9e43f679b24365820ce029874ab
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-07-20 03:31:20 +0300

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -1,13 +1,73 @@
+import random
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+import insert_order
+from insert_order import order_obj
+
 
 from models import Base, Order
+
 engine = create_engine('sqlite:///orders.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 def process_order(order):
-    #Your code here
+    # Your code here
+    orders = session.query(Order).filter(Order.filled == None).all()
+    sender_stored = order['sender_pk'] # store the sender_pk to be able to retrieve the order from the db later
+    insert_order_to_db(order)
+    for existing_order in orders:
+        if match_found(existing_order, order):
+            new_order = session.query(Order).filter(Order.filled == None,Order.sender_pk == sender_stored).first() # retrieve here
+            new_order.counterparty_id = existing_order.id
+            existing_order.counterparty_id = new_order.id
+            existing_order.filled = datetime.now()
+            new_order.filled = datetime.now()
+            session.commit()
     pass
+
+
+def match_found(existing_order, order):
+    total_conditions = 4
+    conditions_satisfied = 0
+    if existing_order.buy_currency == order['sell_currency']:
+        conditions_satisfied += 1
+    if existing_order.sell_currency == order['buy_currency']:
+        conditions_satisfied += 1
+    if order['sell_amount'] != 0 and existing_order.buy_amount != 0:
+        if existing_order.sell_amount / existing_order.buy_amount >= order['buy_amount'] / order['sell_amount']:
+            conditions_satisfied += 1
+    if order['buy_amount'] == existing_order.sell_amount:
+        conditions_satisfied += 1
+
+    if conditions_satisfied == total_conditions:
+        return True
+    else:
+        return False
+
+
+def insert_order_to_db(order):
+    fields = ['sender_pk', 'receiver_pk', 'buy_currency', 'sell_currency', 'buy_amount', 'sell_amount']
+    order_obj = Order(**{f: order[f] for f in fields})
+    session.add(order_obj)
+    session.commit()
+    print("order inserted ", order)
+
+
+if __name__ == '__main__':
+    order = {
+        'buy_currency': "Algorand",
+        'sell_currency': "Ethereum",
+        #'buy_amount': 1245.00,
+        #'sell_amount': 2342.31,
+        'buy_amount': 765,
+        'sell_amount': 3,
+        'sender_pk': 'AAAAC3NzaC1lZDI1NjE5AAAAIB8Ht8Z3j6yDWPBHQtOp/R9rjWvfMYo3MSA/K6q8D86r',
+        'receiver_pk': '0xd1B77a920A0c5010469F40f14c5e4E03f4357226'
+    }
+    process_order(order)
+    exit()
