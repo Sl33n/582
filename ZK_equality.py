50cf500f9ba5eb3e8e51b088cb6b2156d53166d0
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-07-27 00:53:37 +0300

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -8,5 +8,11 @@ def ZK_equality(G,H):
     #Generate a NIZK proving equality of the plaintexts
 
     #Return two ciphertexts and the proof
+    C1 = 0
+    C2 = 0 
+    D1 = 0
+    D2 = 0
+    zk_proof = 0
+    
     return (C1,C2), (D1,D2), zk_proof
 
