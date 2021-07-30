8ced1123b48c381cbc2306a19339d0f379d399f8
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-07-30 07:20:47 +0300

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -15,9 +15,8 @@ def ZK_equality(G, H):
     D1 = r2.value * G
     D2 = m.value * G + r2.value * H
 
-    stmt = DLRep(C1, r1.value * G) & DLRep(C2, r1.value * H + m.value * G) & DLRep(D1, r2.value * G) & DLRep(D2, r2.value * H + m.value * G)
+    stmt = DLRep(C1,r1.value*G) & DLRep(C2,r1.value*H+m.value*G) & DLRep(D1,r2.value*G) & DLRep(D2,r2.value*H+m.value*G)
     zk_proof = stmt.prove()
-    assert stmt.verify(zk_proof)
     # Generate a NIZK proving equality of the plaintexts
 
     # Return two ciphertexts and the proof
