f25dcfcea4dd715fcf6a367f393e6dae61ffff5c
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-07-30 06:59:42 +0300

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -10,10 +10,10 @@ def ZK_equality(G, H):
     m = Secret(utils.get_random_num(bits=128))
 
     C1 = r1.value * G
-    C2 = m * G + r1.value * H
+    C2 = m.value * G + r1.value * H
 
     D1 = r2.value * G
-    D2 = m * G + r2.value * H
+    D2 = m.value * G + r2.value * H
 
     stmt = DLRep(C1, r1.value * G) & DLRep(C2, r1.value * H + m.value * G) & DLRep(D1, r2.value * G) & DLRep(D2, r2.value * H + m.value * G)
     zk_proof = stmt.prove()
