7287d268826e4d01330c45e9849997920099c11f
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-07-30 09:31:45 +0300

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -14,7 +14,8 @@ def ZK_equality(G, H):
     D1 = r2.value * G
     D2 = m * G + r2.value * H
 
-    stmt = DLRep(C1,r1.value * G) & DLRep(C2,r1.value * H + m * G) & DLRep(D1,r2.value * G) & DLRep(D2,r2.value * H + m*G)
+    stmt = DLRep(C1,r1.value * G) & DLRep(C2,r1.value * H + m.value * G) & DLRep(D1,r2.value * G) & DLRep(D2,r2.value * H + m.value * G)
     zk_proof = stmt.prove()
 
     return (C1, C2), (D1, D2), zk_proof
+
