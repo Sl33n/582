4e9ed9d3eeea923297bcf0103377fb2855ffbad4
Sl33n <52133773+Sl33n@users.noreply.github.com>
2021-08-17 06:43:41 +0300

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -1,42 +1,75 @@
 #!/usr/bin/python3
-
 from algosdk.v2client import algod
-from algosdk import mnemonic
-from algosdk import transaction
+from algosdk.v2client import indexer
+from algosdk import account, mnemonic
+from algosdk.future import transaction
 
-#Connect to Algorand node maintained by PureStake
-#Connect to Algorand node maintained by PureStake
-algod_address = "https://testnet-algorand.api.purestake.io/ps2"
-algod_token = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
-#algod_token = 'IwMysN3FSZ8zGVaQnoUIJ9RXolbQ5nRY62JRqF2H'
-headers = {
-   "X-API-Key": algod_token,
-}
+def connect_to_algo(connection_type=''):
+    #Connect to Algorand node maintained by PureStake
+    algod_token = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
+    
+    if connection_type == "indexer":
+        # TODO: return an instance of the v2client indexer. This is used for checking payments for tx_id's
+        algod_address = "https://testnet-algorand.api.purestake.io/idx2"
+        headers = {
+            "X-API-Key": algod_token,
+        }
+        algod_client = algod.AlgodClient(algod_token, algod_address, headers)
+        return algod_client
 
-acl = algod.AlgodClient(algod_token, algod_address, headers)
-min_balance = 100000 #https://developer.algorand.org/docs/features/accounts/#minimum-balance
+    else:
+        # TODO: return an instance of the client for sending transactions
+        # Tutorial Link: https://developer.algorand.org/tutorials/creating-python-transaction-purestake-api/
+        algod_address = "https://testnet-algorand.api.purestake.io/ps2"
+        purestake_token = {'X-Api-key': algod_token}
+        mnemonic_secret = "opinion luxury security private power sponsor coast explain fault project gossip garden cupboard fly squirrel motion drop estate decade symptom lion danger smoke abandon green"
+        account_private_key = mnemonic.to_private_key(mnemonic_secret)
+        account_public_key = mnemonic.to_public_key(mnemonic_secret)
+        algod_client = algod.AlgodClient(algod_token, algod_address, headers=purestake_token)
+        return algod_client
 
-def send_tokens( receiver_pk, tx_amount ):
-    params = acl.suggested_params()
+def send_tokens_algo( acl, sender_sk, txes):
+    params = acl.suggested_params
     gen_hash = params.gh
     first_valid_round = params.first
     tx_fee = params.min_fee
     last_valid_round = params.last
-    sender_pk = '4EHA2QPRXC2ZJDIAGSKHPSAACC2UC4TGFBKL3TL4QXB2KJZGJAOYZEUC7E'
 
-    account_private_key = '388fvWgfXxIqhr1/ztNnQ29U0YKD40ddhfc22fiN3ZvhDg1B8bi1lI0ANJR3yAAQtUFyZihUvc18hcOlJyZIHQ=='
+    
+    # TODO: You might want to adjust the first/last valid rounds in the suggested_params
+    #       See guide for details
 
-    tx = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver_pk,
-                                tx_amount, flat_fee=True)
-    signed_tx = tx.sign(account_private_key)
+    # TODO: For each transaction, do the following:
+    #       - Create the Payment transaction 
+    #       - Sign the transaction
+    
+    # TODO: Return a list of transaction id's
 
-    tx_confirm = acl.send_transaction(signed_tx)
-    wait_for_confirmation(acl, txid=signed_tx.transaction.get_txid())
-    txid=signed_tx.transaction.get_txid()
-    return sender_pk, txid
+    sender_pk = account.address_from_private_key(sender_sk)
+
+    tx_ids = []
+    for i,tx in enumerate(txes):
+
+        # TODO: Sign the transaction
+        signed_tx = tx.sign(sender_pk)
+        
+        try:
+            print(f"Sending {tx['amount']} microalgo from {sender_pk} to {tx['receiver_pk']}" )
+            
+            # TODO: Send the transaction to the testnet
+            
+            txinfo = wait_for_confirmation_algo(acl, txid=tx_id )
+            tx_id = signed_tx.transaction.get_txid()
+            tx_ids.append(tx_id)
+
+            print(f"Sent {tx['amount']} microalgo in transaction: {tx_id}\n" )
+        except Exception as e:
+            print(e)
+
+    return tx_ids
 
 # Function from Algorand Inc.
-def wait_for_confirmation(client, txid):
+def wait_for_confirmation_algo(client, txid):
     """
     Utility function to wait until the transaction is
     confirmed before proceeding.
@@ -51,3 +84,55 @@ def wait_for_confirmation(client, txid):
     print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
     return txinfo
 
+##################################
+
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from web3.exceptions import TransactionNotFound
+import json
+import progressbar
+
+
+def connect_to_eth():
+    IP_ADDR='3.23.118.2' #Private Ethereum
+    PORT='8545'
+
+    w3 = Web3(Web3.HTTPProvider('http://' + IP_ADDR + ':' + PORT))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0) #Required to work on a PoA chain (like our private network)
+    w3.eth.account.enable_unaudited_hdwallet_features()
+    if w3.isConnected():
+        return w3
+    else:
+        print( "Failed to connect to Eth" )
+        return None
+
+def wait_for_confirmation_eth(w3, tx_hash):
+    print( "Waiting for confirmation" )
+    widgets = [progressbar.BouncingBar(marker=progressbar.RotatingMarker(), fill_left=False)]
+    i = 0
+    with progressbar.ProgressBar(widgets=widgets, term_width=1) as progress:
+        while True:
+            i += 1
+            progress.update(i)
+            try:
+                receipt = w3.eth.get_transaction_receipt(tx_hash)
+            except TransactionNotFound:
+                continue
+            break 
+    return receipt
+
+
+####################
+def send_tokens_eth(w3,sender_sk,txes):
+    sender_account = w3.eth.account.privateKeyToAccount(sender_sk)
+    sender_pk = sender_account._address
+
+    # TODO: For each of the txes, sign and send them to the testnet
+    # Make sure you track the nonce -locally-
+    
+    tx_ids = []
+    for i,tx in enumerate(txes):
+        # Your code here
+        continue
+
+    return tx_ids
